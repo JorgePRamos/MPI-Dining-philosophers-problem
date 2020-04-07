d3e940e93770551a89b6ddd4acaa7ae5e458a200
Jorge Pérez Ramos <jorgeperezramos99@gmail.com>
2020-04-07 13:54:14 +0200

    Mainly functional; Cant stop process

--- MPI_Dining philosophers problem/Main.cpp
+++ MPI_Dining philosophers problem/Main.cpp
@@ -21,68 +21,81 @@ void philosopher(int myRank) {
 	//Philosopher main loop
 	while (true) {
 
-		Sleep(rand() % 10); //Think
+		//Sleep(rand() % 10); //Think
+		Sleep(1);
 		MPI_Send(&buffer, 1, MPI_INT, 0, REQUEST, MPI_COMM_WORLD); //Request forks
+		printf("Im philo n %d ## Wait for resposed\n", myRank);
 		MPI_Recv(&buffer, 1, MPI_INT, 0, RESPONSE, MPI_COMM_WORLD, &stat);//wait response
-		Sleep(rand() % 10);//Eat
-		MPI_Send(&buffer, 1, MPI_INT, 0, RELEASE, MPI_COMM_WORLD);//Release forks
+		printf("Im philo n %d ## COMO\n", myRank);
+		//Sleep(rand() % 10);//Eat
+		Sleep(1);
 
+		MPI_Send(&buffer, 1, MPI_INT, 0, RELEASE, MPI_COMM_WORLD);//Release forks
+		printf("Im philo n %d ## Dejo los tenedores\n", myRank);
 	}
 }
 
 
 
-void table(int myrank, int nprocs) {
-	printf("Hello from table %d \n", myrank);
+void monitor(int myrank, int nprocs) {
+	printf("Hello from monitor %d \n", myrank);
 	int buffer;
-
-	int philosopher;
+	
+	int philosopherRank;
 	MPI_Status stat;
 
-	//std::list<int> queue;
+
 	queue<int> queue;
 	
 
 	bool forks[5] = { true ,true ,true ,true , true };//SUBSTRAER 1 POR QUE EL 0 ES EL MONITOR
-	for (int i = 0; i < nprocs - 1; i++) forks[i] = true; //Init all forks as free
+	
 
 	while (true) {
 
 
 		MPI_Recv(&buffer, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &stat); // Recive next message
-		philosopher = stat.MPI_SOURCE; //Read source of message NUMBER OF PHIL
+		philosopherRank = stat.MPI_SOURCE; //Read source of message NUMBER OF PHIL
 
 
 
 		if (stat.MPI_TAG == RELEASE) { //If Release of forks ***************POSIBLE PROBLEMA****
-		forks[philosopher % (nprocs - 1)] = true; //Set forks to free again
-		forks[philosopher - 1] = true;
+	
+
+		forks[(((philosopherRank - 1) % (nprocs - 1)) + 1) % (nprocs - 1)] = true; //Set the forks as taken
+		forks[philosopherRank - 1] = true;
 
 	
 		}else if (queue.empty()) {//We take new
-
+				//mine (rank -1)
+				//next (((rank-1)mod (nProc-1))+1) mod (nProc-1)
+			//forks[(((philosopherRank - 1)%(nprocs - 1)) + 1) %(nprocs - 1)] == true && forks[philosopherRank - 1] == true
 
 				if (stat.MPI_TAG == REQUEST) { //If Request for forks
-					if (forks[philosopher % (nprocs - 1)] == true && forks[philosopher - 1] == true) { //If both forks are free
-						forks[philosopher % (nprocs - 1)] = false; //Set the forks as taken
-						forks[philosopher - 1] = false;
-						MPI_Send(&buffer, 1, MPI_INT, philosopher, RESPONSE, MPI_COMM_WORLD); // Send Fork response to the right philosopher
+					if (forks[(((philosopherRank - 1)%(nprocs - 1)) + 1) %(nprocs - 1)] == true && forks[philosopherRank - 1] == true) { //If both forks are free
+						printf("Im philo n %d ## TAKING FORKS\n", philosopherRank);
+						forks[(((philosopherRank - 1) % (nprocs - 1)) + 1) % (nprocs - 1)] = false; //Set the forks as taken
+						forks[philosopherRank - 1] = false;
+						MPI_Send(&buffer, 1, MPI_INT, philosopherRank, RESPONSE, MPI_COMM_WORLD); // Send Fork response to the right philosopher
 
 					}
 					else {//If not both forks are free
-						queue.push(philosopher); //Put in wait queue
+						printf("Im philo n %d ## GOING IN TO QUEUE\n", philosopherRank);						
+						queue.push(philosopherRank); //Put in wait queue
 					}
 
 				}
 
 
 		}else {
-				queue.push(philosopher); //Put in wait queue  the newCommer
-				philosopher = (int)(queue.front());
+				queue.push(philosopherRank); //Put in wait queue  the newCommer
+				philosopherRank = (int)(queue.front());
+				printf("Im philo n %d ## COMING OUT OF QUEUE\n", philosopherRank);
 				queue.pop();
-				forks[philosopher % (nprocs - 1)] = false; //Set the forks as taken
-				forks[philosopher - 1] = false;
-				MPI_Send(&buffer, 1, MPI_INT, philosopher, RESPONSE, MPI_COMM_WORLD); // Send Fork response to the right philosopher
+				forks[(((philosopherRank - 1) % (nprocs - 1)) + 1) % (nprocs - 1)] = false; //Set the forks as taken
+				forks[philosopherRank - 1] = false;
+				MPI_Send(&buffer, 1, MPI_INT, philosopherRank, RESPONSE, MPI_COMM_WORLD); // Send Fork response to the right philosopher
+
 		}
 
 	}
@@ -90,29 +103,28 @@ void table(int myrank, int nprocs) {
 
 int main(int argc, char** argv) {
 	enum role { MONITOR, PHILOSOPHER};
-	int myRank, nprocs;
-	//init MPI
+	int myRank, comm_size;
+	
 	MPI_Init(&argc, &argv);
-	MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
 	MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
-	printf("say HI.  %d", argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
+
 
 	switch (myRank)
 	{
 	case MONITOR:
 	{
-		//ble(myRank, nprocs);
+		monitor(myRank, comm_size);
 		break;
 	}
-	case PHILOSOPHER:
+	default:
 	{
 		philosopher(myRank);
 		break;
 	}
 	}
-	//Depending on rand, Philosopher or Table
-	//if (myRank == 0) table(myRank, nprocs);
-//	else philosopher(myRank);
+
 
 	MPI_Finalize();
 	return 0;
